3857b85b3f72dae99129fa20424facd1c4f99d61
shawnicesloan <ssloan22@scienceleadership.org>
2021-01-20 06:35:21 -0500

    I just want to make sure everything I added is good w you

--- src/org/sla/Company.java
+++ src/org/sla/Company.java
@@ -1,15 +1,20 @@
 package org.sla;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Company extends Business{
+
     private String Country;
     private double profit;
     private String Industry;
 
     Company(int rank, String name, double revenue, int employeeCt, String country, float profit, String industry) {
         super(rank, name, revenue, employeeCt);
-        Country = country;
+        this.Country = country;
         this.profit = profit;
-        Industry = industry;
+        this.Industry = industry;
     }
 
     String getCountry() {
@@ -39,7 +44,40 @@ class Company extends Business{
 
     public String toString() {
         String desc = "";
-        desc += "Company: " + this.getName() + ", " + this.getIndustry() + ", " + this.getCountry();
+        desc += "Company: " + this.getName() + ", belongs to the " + this.getIndustry() + " industry, but is home to " + this.getCountry();
+        desc += "With" + this.getEmployeeCt() + " employees," + this.getName() + "earns" + this.getRevenue() + "in revenue, but only makes a profit of" + this.getProfit();
+        desc += "Considering the previous data" + this.getName() + " earns a global rank of" + this.getRank();
         return desc;
+    }
+
+    static void read(String dataFilePath) {
+        // try to create Scanner
+        Scanner scanner = null;
+        try {
+            File file = new File(dataFilePath);
+            scanner = new Scanner(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Problem opening file: " + dataFilePath);
+        }
+        if (scanner == null) {
+            return;
+        }
+
+
+        while (scanner.hasNext()) {
+            String next = scanner.nextLine();
+            Scanner lineScanner = new Scanner(next);
+            lineScanner.useDelimiter("\t");
+
+
+            int ranking = lineScanner.nextInt();
+            int peak = lineScanner.nextInt();
+            String title = lineScanner.next();
+            long revenue = lineScanner.nextLong();
+            int year = lineScanner.nextInt();
+
+            BusinessData businessData = new Company(rank, name,revenue,employeeCt, country, profit, industry);
+        }
     }
 }
